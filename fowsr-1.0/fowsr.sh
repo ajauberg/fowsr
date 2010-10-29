638d8bd54254589d53c22fb476109eba8faea88c
arne.jorgen.auberg <arne.jorgen.auberg@fc7da526-431c-80fe-3c8c-c148ff18d7ef>
2010-10-29 19:44:27 +0000

    Added PWS Weather format, added multiple output format option, patch for FreeBSD (thanks Alexander Leidinger)

---
+++ fowsr-1.0/fowsr.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Call fowsr with multiple parameters
+
+PWSLOG=/var/pwsweather.log
+rm -f $PWSLOG
+
+WUGLOG=/var/wunderground.log
+rm -f $WUGLOG
+
+wsr="/usr/bin/fowsr -ws"
+$wsr
+
+# Upload data to Wunderground
+
+ID=$1
+PASSWORD=$2
+WGET=http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php
+WGET="$WGET?action=updateraw&ID=$ID&PASSWORD=$PASSWORD&softwaretype=fowsr&"
+
+while read line
+do
+  WGET2="$WGET`echo $line`"
+  echo $WGET2
+  wget -O /dev/null "$WGET2"
+done < $WUGLOG
+
+# Upload data to PWS Weather
+
+ID=$1
+PASSWORD=$2
+WGET=http://www.pwsweather.com/pwsupdate/pwsupdate.php
+WGET="$WGET?action=updateraw&ID=$ID&PASSWORD=$PASSWORD&softwaretype=fowsr&"
+
+while read line
+do
+  WGET2="$WGET`echo $line`"
+  echo $WGET2
+  wget -O /dev/null "$WGET2"
+done < $PWSLOG
+
---
+++ fowsr-1.0/pwsweather.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Call fowsr and upload data to PWS Weather
+
+wsr="/usr/bin/fowsr -s"
+LOG=/var/pwsweather.log
+ID=$1
+PASSWORD=$2
+
+WGET=http://www.pwsweather.com/pwsupdate/pwsupdate.php
+
+WGET="$WGET?action=updateraw&ID=$ID&PASSWORD=$PASSWORD&softwaretype=fowsr&"
+
+rm -f $LOG
+$wsr
+
+while read line
+do
+  WGET2="$WGET`echo $line`"
+
+  echo $WGET2
+  wget -O /dev/null "$WGET2"
+done < $LOG
+
